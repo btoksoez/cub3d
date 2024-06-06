7ba8ed15dfecb16b925de0842f65ed4dd08f08b8
Andre Albuquerque <andrealbuquerque@iMac.home>
2024-06-06 14:57:17 +0100

    updates

--- sources/3.graphics/init_mlx.c
+++ sources/3.graphics/init_mlx.c
@@ -25,7 +25,7 @@ void	init_game_struct(t_game *game, t_player *player, t_map *map)
 	player->pos.x = map->player.x * SCALE + 3 * (PSIZE / 2);
 	player->pos.y = map->player.y * SCALE + 3 * (PSIZE / 2);
 	player->look_dir = 0;
-	player->speed = 2;
+	player->speed = 1;
 	player->jump_height = 0;
 	player->jumping = false;
 	player->prev_mouse_x = 0;
